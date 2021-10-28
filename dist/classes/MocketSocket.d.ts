30d547b7d115e7a022ffac14a1d7f1ad8dba4ce3
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-10-28 13:00:57 +0200

    update package.json

--- dist/classes/MocketSocket.d.ts
+++ dist/classes/MocketSocket.d.ts
@@ -1,7 +1,11 @@
 export declare class MocketSocket {
     rooms: Set<string>;
+    joinedRooms: Set<string>;
+    connected: boolean;
+    disconnected: boolean;
     constructor();
     join(rooms: string | Array<string>): void;
     leave(room: string): void;
     leaveAll(): void;
+    disconnect(): this;
 }
--- dist/classes/MocketSocket.js
+++ dist/classes/MocketSocket.js
@@ -4,18 +4,27 @@ exports.MocketSocket = void 0;
 class MocketSocket {
     constructor() {
         this.rooms = new Set();
+        this.joinedRooms = new Set();
+        this.connected = true;
+        this.disconnected = false;
     }
     join(rooms) {
         if (typeof rooms === 'string') {
             rooms = [rooms];
         }
         rooms.forEach(e => this.rooms.add(e));
+        this.joinedRooms.forEach(e => this.rooms.add(e));
     }
     leave(room) {
         this.rooms.delete(room);
     }
     leaveAll() {
         this.rooms.clear();
+    }
+    disconnect() {
+        this.disconnected = true;
+        this.connected = true;
+        return this;
     }
 }
 exports.MocketSocket = MocketSocket;
